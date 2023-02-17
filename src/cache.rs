93362b9b4c957a2390c6c91d6c1ebe5d1d49688e
Charl <charl.kivioja@nortal.com>
2023-02-17 13:49:46 +0200

    remove RAM usage pt1

--- src/cache.rs
+++ src/cache.rs
@@ -21,6 +21,9 @@ impl DBOperations for Database {
         opts.set_use_direct_io_for_flush_and_compaction(true);
         opts.set_compaction_readahead_size(2 * 1024 * 1024); // 2MB
         opts.set_writable_file_max_buffer_size(1024 * 1024); // 1MB
+        opts.set_level_zero_file_num_compaction_trigger(-1);
+        opts.set_max_write_buffer_number(0);
+        opts.set_write_buffer_size(0);
         return Database {         
             inst: Arc::new(DB::open(&opts, file_path).unwrap()),
         };
