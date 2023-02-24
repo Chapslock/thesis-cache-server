f36324005fd0f0111fc70a522da0944d723fd095
Charl <charl.kivioja@nortal.com>
2023-02-24 23:00:52 +0200

    remove 3GB file temporarily

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ pub static CACHE_DATABASE_PATH: &str = "./database";
 pub static SERVER_PORT: u16 = 8080;
 pub static PATH_TO_TEST_FILES: &str = "/var/www/html/";
 //static PATH_TO_TEST_FILES: &str = "C:\\Users\\Charl.Kivioja\\Desktop\\http-test-server\\testFiles\\";
-pub static TEST_FILES: [&str; 5] = ["500KB.html", "1MB.html", "10MB.html", "100MB.html", "3GB.html"];
+pub static TEST_FILES: [&str; 4] = ["500KB.html", "1MB.html", "10MB.html", "100MB.html"];
 //static TEST_FILES: [&str; 1] = ["500KB.html"];
 
 #[tokio::main]
